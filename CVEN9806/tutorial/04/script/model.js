8c6beec2bc94d803754433f3ad7fdf79fdda0048
James Chen <edwinatrus@gmail.com>
2018-01-29 09:47:55 +1100

    Add files via upload

--- CVEN9806/tutorial/04/script/model.js
+++ CVEN9806/tutorial/04/script/model.js
@@ -1,23 +1,109 @@
 class Model {
-    constructor() {
-        this.gs = 2.7;
-        this.d1 = 2
-        this.d2 = 5;
-        this.d3 = 2;
+    constructor(wf, hf, ww, hw, d0) {
+        this.wf = wf;
+        this.hf = hf;
+        this.ww = ww;
+        this.hw = hw;
         
-        this.boundingbox = [-1, 2, 11, -11];
+        this.d0 = d0;
         
-        this.plot = {};
+        this.span = 40;
+        this.load = 22;
         
-        this.q = 2000;
-        this.b = 1;
-        this.l = 1;
+        this.fcp = 40;
+        this.fc = 40;
+        this.fsy = 500;
         
-        this.gamma1 = 20;
-        this.gamma2 = 16;
+        this.es = 2e5;
         
-        this.cc = 0.5;
-        this.cr = 0.2;
-        this.pc = 54;
+        this.as = 1880;
+        this.ap = 5428;
+        
+        this.pt = 7738;
+        this.pe = 6500;
+        
+        this.duct = 140;
+        
+        this.check = 1;
+    }
+    
+    get a() {  // total area
+        return this.wf*this.hf*2 + this.ww*this.hw;
+    }
+    
+    get sw() {  // self weight
+        return this.a / 1e6 * 24;
+    }
+    
+    get w() {  // total load
+        var result = 1.2*this.sw + 1.5*this.load;
+        
+        return Number(result.toFixed(0));
+    }
+    
+    get vmax() {
+        return this.w * this.span / 2;
+    }
+    
+    get mmax() {
+        return this.w * this.span * this.span / 8;
+    }
+    
+    get v() {  // factored designed shear force
+        return this.vmax - this.w*this.check;
+    }
+    
+    get m() {  // factored designed moment
+        return this.vmax*this.check - this.w*this.check*this.check/2
+    }
+    
+    y(x) {  // displacement of tendon
+        var e = this.hw / 2;
+        var l = this.span * 1000;
+        var result = 4 * e * (x*1000/l - Math.pow(x*1000/l, 2));
+        
+        return Number(result.toFixed(2));
+    }
+    
+    y_p(x) {  // slope of tendon
+        var e = this.hw / 2;
+        var l = this.span * 1000;
+        var result = 4 * e / l * (1 - 2*x*1000/l);
+        
+        return Number(result.toFixed(4));
+    }
+    
+    pv(x) {  // vertical component of pe
+        var result = this.pe * this.y_p(x);
+        
+        return Number(result.toFixed(0));
+    }
+    
+    /**
+     * CHECKING SHEAR CRACKING AT X
+     */
+    get i() {  // second moment of inertia
+        var result = this.wf * Math.pow(2*this.hf + this.hw, 3) / 12;
+        result -= (this.wf - this.ww) * Math.pow(this.hw, 3) / 12;
+        
+        return Number(result.toExponential(4));
+    }
+    
+    m0(x) {
+        var e = this.y(x);
+        x *= 1000;
+        var zb = this.i / x;
+        
+        var result = zb * (this.pe*1000/this.a + this.pe*1000*e/zb);
+        
+        return Number((result / 1e6).toFixed(1));
+    }
+    
+    v0(x) {
+        var m0 = this.m0(x);
+        
+        var result = m0 * this.v / this.m;
+        
+        return Number(result.toFixed(1));
     }
 }
--- CVEN9806/tutorial/04/script/vm.js
+++ CVEN9806/tutorial/04/script/vm.js
@@ -1,45 +1,8 @@
-var prob = new Model();
+var prob = new Model(800, 300, 350, 1400, 1900);
 
 var vm = new Vue({
     el: "#myapp",
-    
     data: {
-        prob: prob,
-        d2: 5
-    },
-    
-    computed: {
-        sigma_0: function() {
-            var r = this.prob.gamma1*this.prob.d1 + this.prob.gamma2*this.d2/2 - this.d2/2*9.8;
-            
-            r = Number(r.toFixed(2));
-            
-            return r;
-        },
-        
-        sigma_delta: function() {
-            var r = this.prob.q * this.prob.b * this.prob.l;
-            r /= this.prob.b + this.prob.d1 + this.d2;
-            r /= this.prob.l + this.prob.d1 + this.d2;
-            r = Number(r.toFixed(2));
-            
-            return r;
-        },
-        
-        sigma_f: function() {
-            var r0 = this.prob.gamma1*this.prob.d1 + this.prob.gamma2*this.d2/2 - this.d2/2*9.8;
-            
-            var r = this.prob.q * this.prob.b * this.prob.l;
-            r /= this.prob.b + this.prob.d1 + this.d2;
-            r /= this.prob.l + this.prob.d1 + this.d2;
-            
-            var r1 = Number((r + r0).toFixed(2));
-            
-            return r1;
-        }
-    },
-    
-    methods: {
-        
+        prob: prob
     }
 });
