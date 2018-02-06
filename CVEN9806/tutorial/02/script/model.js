5881c9f978f1b3d1db6bd9e51b86ccccff0a8a2b
James Chen <edwinatrus@gmail.com>
2018-02-07 09:45:05 +1100

    Add files via upload

--- CVEN9806/tutorial/02/script/model.js
+++ CVEN9806/tutorial/02/script/model.js
@@ -1,7 +1,20 @@
-class Model {
-    constructor(span, e) {
-        this.span = span;
-        this.e = e;
-        this.boundingbox = [-1, 500, this.span + 2, -700];
+class Problem {
+    constructor() {
+        this.b = 1200;
+        this.span = 4;
+        
+        this.ll = 6;
+        
+        this.R = 0.8;
+        
+        this.dia = 7;
+        this.cover = 20;
+        
+        this.break = 65.5;
+        
+        this.fti = 1.4
+        this.ft = 1.4;
+        this.fci = -16;
+        this.fc = -16;
     }
 }
