9757c06b6469dda9b28f1e3f87f760060cb6e1f5
James Chen <edwinatrus@gmail.com>
2018-02-16 10:46:21 +1100

    Add files via upload

--- CVEN3202/05/script/event.js
+++ CVEN3202/05/script/event.js
@@ -1,15 +1,5 @@
 function init() {
-    $("html").on("contextmenu", function(e) {
-        (new Audio('css/warning.wav')).play();
-        
-        /*
-        setTimeout(function() {
-            alert("Right-click is disabled");
-        }, 100);
-        */
-        
-        return false;
-    });
+    disableInspect('css/warning.wav');
     
     $("#tabs").tabs();
     plot_figure(vm.h1, vm.h2, vm.l1, vm.l2, vm.l3, vm.angle, true);
