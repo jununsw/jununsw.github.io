d699f1db5e9baee225b8d473a0667394174c5277
James Chen <edwinatrus@gmail.com>
2018-09-04 09:41:51 +1000

    Update util.js

--- res/util.js
+++ res/util.js
@@ -255,3 +255,19 @@ function toPost(path, params, method) {
     document.body.appendChild(form);
     form.submit();
 }
+
+function getParameterByName(name, url) {
+    if (!url) {
+	    url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)');
+    var results = regex.exec(url);
+    if (!results) {
+	    return null;
+    }
+    if (!results[2]) {
+	    return '';
+    }
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
