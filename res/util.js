28baf3c0dee4b642810faac5fad09ee12e8b96f2
James Chen <edwinatrus@gmail.com>
2019-03-22 13:38:46 +1100

    Add files via upload

--- res/util.js
+++ res/util.js
@@ -130,51 +130,17 @@ function disableInspect(path) {
     });
 }
 
-function disableCopy() {
-    document.onkeydown = function(e) {
-        if (event.keyCode == 123) {
-            return false;
-        }
-        
-        if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-            return false;
-        }
-        
-        if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-            return false;
-        }
-        
-        if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-            return false;
-        }
-
-        if (e.ctrlKey && e.keyCode == 'C'.charCodeAt(0)) {
-            return false;
-        }
-
-        if (e.ctrlKey && e.keyCode == 'V'.charCodeAt(0)) {
-            return false;
-        }
-
-        if (e.ctrlKey && e.keyCode == 'X'.charCodeAt(0)) {
-            return false;
-        }
-    }
-    
-    $("html").on("contextmenu", function(e) {
-        return false;
-    });
-}
-
 function disableKeyboard() {
     document.onkeydown = function(e) {
-        if ((event.keyCode >= 30) && (event.keyCode <= 39)) {
+        if ((event.keyCode >= '0'.charCodeAt(0)) && (event.keyCode <= '9'.charCodeAt(0))) {
             
         } else if (e.keyCode == '.'.charCodeAt(0)) {
 
         } else if (e.keyCode == 'E'.charCodeAt(0)) {
 
         } else if (e.keyCode == 'e'.charCodeAt(0)) {
+
+        } else if (e.keyCode == 8) {
 
         } else {
             return false;
